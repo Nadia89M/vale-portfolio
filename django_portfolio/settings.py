33903c2881c2e14059b44f9f9a5016943c141d07
Nadia89M <nadia.mohamed89@gmail.com>
2019-03-23 18:23:23 +0000

    Update settings.py as Images uploaded with ckeditor are not displaying

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -158,6 +158,8 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = 'eu-west-2'
 
+AWS_S3_BUCKET_AUTH = False
+
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
