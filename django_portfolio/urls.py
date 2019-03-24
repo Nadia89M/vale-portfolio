183aa8f8263bcee4ff355db16e1a7203725cfb43
Nadia89M <nadia.mohamed89@gmail.com>
2019-03-24 14:57:09 +0000

    Update urls.py

--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -25,9 +25,5 @@ urlpatterns = [
     path('blog/', include('posts.urls')),
     path('portfolio/', include('categories.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-] 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += [
-        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    ]
